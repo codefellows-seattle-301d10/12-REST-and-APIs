5cb365be74e0f1f47d8044738fb410e4926b4fce
Rick Patci <rpatci@gmail.com>
2016-07-25 23:45:05 -0700

    push starter-code

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -1,18 +1,18 @@
 (function(module) {
-  var repos = {};
+  var reposObj = {};
 
-  repos.allRepos = [];
+  reposObj.allRepos = [];
 
-  repos.requestRepos = function(callback) {
+  reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
   };
 
-  repos.withTheAttribute = function(myAttr) {
-    return repos.allRepos.filter(function(aRepo) {
+  reposObj.withTheAttribute = function(myAttr) {
+    return reposObj.allRepos.filter(function(aRepo) {
       return aRepo[myAttr];
     });
   };
 
-  module.repos = repos;
+  module.reposObj = reposObj;
 })(window);
--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -7,12 +7,12 @@
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
-      repos.withTheAttribute('name')  // TODO: experiment changing this attribute field!
+      reposObj.withTheAttribute('name')  // TODO: experiment changing this attribute!
       .map(repoCompiler)
     );
   };
 /* TODO: Call the function that loads (or 'requests') our repo data.
-    Pass in some view funcyion as a higher order callback, so our repos
+    Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
 
 });
