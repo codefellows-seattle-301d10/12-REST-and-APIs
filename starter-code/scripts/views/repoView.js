866bd48c0ec64ed922f8165e63d2a80c626c832b
Britt Dawn <brittdawndev@gmail.com>
2016-08-30 15:34:48 -0700

    Calling the function that loads our repo data and putting the repoView into an IIFE.

--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -14,5 +14,6 @@
 /* TODO: Call the function that loads (or 'requests') our repo data.
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
-
-});
+  reposObj.requestRepos(repoView.renderRepos);
+  module.repoView = repoView;
+})(window);
