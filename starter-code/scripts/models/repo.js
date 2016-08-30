995360f6b919740c31e8dfa40caf90b056485280
bronwyndet <wayward@waywardcoffee.com>
2016-08-30 12:54:31 -0700

    first few todos completed

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,16 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/codefellows-seattle-301d10' +
+        '?per_page=5' +
+        '&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data) {
+        console.log(data);
+      }
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
