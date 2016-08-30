a2c007d83c0a54c172accbad11d29626ac7dab7d
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-08-30 13:45:14 -0700

    testing

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -7,6 +7,17 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/codefellows-seattle-301d10/repos' +
+            '?per_page=5' +
+            '&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data) {
+        console.log(data);
+        callback();
+      }
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -14,5 +14,5 @@
 /* TODO: Call the function that loads (or 'requests') our repo data.
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
-
+  reposObj.requestRepos(repoView.renderRepos);
 });
