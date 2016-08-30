d35b21b9bb11945165010bec5d0c4f676c2b5244
Britt Dawn <brittdawndev@gmail.com>
2016-08-30 15:48:41 -0700

    Experimented with forks and name as attributes.

--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -7,7 +7,7 @@
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
-      reposObj.withTheAttribute('name')  // TODO: experiment changing this attribute!
+      reposObj.withTheAttribute('name')  // TODO: DONE - experiment changing this attribute!
       .map(repoCompiler)
     );
   };
