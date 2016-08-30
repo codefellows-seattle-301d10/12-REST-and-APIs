2789c5586223a95931c81d9304cd7f6440ac63a0
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-08-30 13:50:51 -0700

    created ajax function to collect repos

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -5,7 +5,7 @@
 // TODO: DONE create a githubToken.js file that we can use to generate our headers
          // properly.
   reposObj.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
+    /* TODO: DONE How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
     $.ajax({
       url: 'https://api.github.com/users/codefellows-seattle-301d10/repos' +
--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -3,7 +3,7 @@
   /* TODO: Let's compile our new template!
        Save the result of invoking Handlebars in this 'repoCompiler' variable
        that we will pass to the append method below. */
-  var repoCompiler;  // Finish the Handlebars method here!
+  var repoCompiler = function(){};  // Finish the Handlebars method here!
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
@@ -11,8 +11,9 @@
       .map(repoCompiler)
     );
   };
-/* TODO: Call the function that loads (or 'requests') our repo data.
+/* TODO: DONE Call the function that loads (or 'requests') our repo data.
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
   reposObj.requestRepos(repoView.renderRepos);
-});
+  module.repoView = repoView;
+})(window);
